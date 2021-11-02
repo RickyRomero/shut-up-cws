dcb938dbb545ded2a123ea5747da46bcddd835b1
Ricky Romero <ricky.romero@gmail.com>
2021-11-01 23:43:21 -0700

    Fix baffling build hang from fs-extra getting stuck copying

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
+
+echo "Setting options to prevent cascading errors..."
 set -eu
 
+echo "Copying extension files..."
 cd /usr/app
 cp -R $GITHUB_WORKSPACE/* /cws/extension/
 
+echo "Preparing build directory..."
+mkdir "/cws/build/Shut Up"
+cp -R /cws/extension/* "/cws/build/Shut Up/"
+ls -la "/cws/build/Shut Up/"
+
+echo "Removing dotfiles..."
+find "/cws/build/Shut Up/" -name ".*" -exec rm -r {} \; -print
+
+echo "Building extension..."
 yarn build
+
+echo "Submitting extension..."
 yarn submit
--- src/build.js
+++ src/build.js
@@ -1,32 +1,15 @@
 const archiver = require('archiver')
-const fs = require('fs-extra')
+const fs = require('fs')
 const path = require('path')
 const pathConfig = require('./paths.json')
 
 const build = async () => {
-  const srcPath = path.resolve(__dirname, pathConfig.sourceDir)
   const destPath = path.resolve(__dirname, pathConfig.artifactsDir)
-  const destFolder = path.join(destPath, 'Shut Up')
-
-  await fs.copy(srcPath, destFolder, {
-    overwrite: true,
-    preserveTimestamps: true,
-    // Remove dotfiles
-    filter: path => {
-      const keepPath = path.split('/').filter(item => /^\./.test(item)).length === 0
-      if (!keepPath) {
-        console.log(`Filtering path: ${path}`)
-      }
-      return keepPath
-    }
-  })
-
   const output = fs.createWriteStream(path.join(destPath, 'Shut Up.zip'))
   const archive = archiver('zip', { zlib: { level: 9 } })
 
   output.on('close', () => {
     console.log(`Compressed payload is ${archive.pointer()} bytes.`)
-    fs.remove(destFolder)
   })
 
   archive.on('error', err => {
